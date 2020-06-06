c973e014b5d3ad102c15d7857a75bb6856610dfc
Renerine <milionevincent@gmail.com>
2020-06-06 22:15:38 +0200

    first steps to js for loading tables

--- src/main/resources/static/js/loadTable.js
+++ src/main/resources/static/js/loadTable.js
@@ -1,6 +1,21 @@
-(function(){
+$( document ).ready(function() {
     let list = new Register().heartBeatLog();
+    
     list.forEach(el => {
+        let tr = $('<tr>');
         
-    })
+        tr.append('<td>'+el.gps+'</td>');
+        tr.append('<td>'+el.name+'</td>');
+        tr.append('<td>'+el.type+'</td>');
+        tr.append('<td>'+el.beat+' bps</td>');
+        tr.append('<td>blabla</td>');
+        tr.css("visibility", "hidden");
+
+        $("#heart").find('tbody').append(tr);
+    });
+
+    $('#heart tr').each(function(){
+        $(this).css("visibility", "visible");
+    });
+    
 });
