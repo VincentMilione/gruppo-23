7162b6d11e5904e035dcd237ece3d805f0a71be2
Renerine <milionevincent@gmail.com>
2020-06-07 16:25:13 +0200

    impl. dei dettagli in heartbeat log e inizio paginazionie

--- src/main/resources/static/js/animal.js
+++ src/main/resources/static/js/animal.js
@@ -2,7 +2,7 @@
 
 //classe animale contiene tutte le info (non c'è bisogno di getters and setters js è un po come python)
 class Animal {
-    constructor(tracker, name, type, bday, sex, lmilk, children, hrtbt, hrtbtH) {
+    constructor(tracker, name, type, bday, sex, lmilk, children, hrtbt, hrtbtH,note) {
         this.type = type;
         this.tracker= tracker;
         this.name= name;
@@ -12,6 +12,7 @@ class Animal {
         this.children= children;
         this.hrtbt= hrtbt;
         this.hrtbtH= hrtbtH;
+        this.note= note;
     }
 }
 
@@ -19,21 +20,21 @@ class Animal {
 class Register {
     constructor () {
         this.list =  [
-            new Animal("q001w","Nuvola","Bovino",new Date(2018,9,9,0,0,0),"Femmina",12,3,70,[70,71,71,81,69,70]),
-            new Animal("q002w","Pistacchio","Bovino",new Date(2017,9,6,0,0,0),"Maschio",0,0,6,69,[70,71,71,81,69,70]),
-            new Animal("q003w","Mandorla","Bovino",new Date(2019,9,6,0,0,0),"Femmina",9,1,71,[70,71,71,81,69,70]),
-            new Animal("q004w","Nocciola","Caprino",new Date(2018,2,6,0,0,0),"Femmina",8,1,71,[70,71,72,71,69,70]),
-            new Animal("q005w","Noce","Caprino",new Date(2017,9,2,0,0,0),"Femmina",14,3,70,[70,71,71,81,69,70]),
-            new Animal("q006w","Neve","Caprino",new Date(2020,4,6,0,0,0),"Femmina",0,0,69,[70,71,71,81,69,70]),
-            new Animal("q007w","Biancaneve","Caprino",new Date(2020,9,1,0,0,0),"Femmina",0,0,70,[70,71,71,81,69,70]),
-            new Animal("q008w","Brontolo","Caprino",new Date(2020,9,6,0,0,0),"Maschio",0,0,70,[70,71,71,81,69,70]),
-            new Animal("q009w","Mammolo","Caprino",new Date(2017,9,6,0,0,0),"Maschio",0,0,72,[70,71,71,81,69,70]),
-            new Animal("q010w","Cenerentola","Ovino",new Date(2017,9,6,0,0,0),"Femmina",13,3,70,[70,71,71,81,69,70]),
-            new Animal("q011w","Alice","Ovino",new Date(2016,9,6,0,0,0),"Femmina",19,4,73,[70,71,71,81,69,70]),
-            new Animal("q012w","Dolly","Ovino",new Date(2017,10,6,0,0,0),"Femmina",23,3,68,[70,71,71,81,69,70]),
-            new Animal("q013w","Aladino","Ovino",new Date(2016,5,1,0,0,0),"Maschio",0,0,0,[70,71,71,81,59,41]),
-            new Animal("q014w","Jasmine","Ovino",new Date(2020,9,6,0,0,0),"Femmina",0,0,70,[70,71,71,81,69,70]),
-            new Animal("q015w","Bianchina","Ovino",new Date(2020,1,6,0,0,0),"Femmina",0,0,70,[70,71,71,81,69,70]),
+            new Animal("q001w","Nuvola","Bovino",new Date(2018,9,9,0,0,0),"Femmina",12,3,70,[70,71,71,81,69,70], "Nuvola ha subito una operazione da poco. Potrebbe avere battiti alti"),
+            new Animal("q002w","Pistacchio","Bovino",new Date(2017,9,6,0,0,0),"Maschio",0,0,69,[70,71,71,81,69,70],"Nessuna nota"),
+            new Animal("q003w","Mandorla","Bovino",new Date(2019,9,6,0,0,0),"Femmina",9,1,71,[70,71,71,81,69,70],"Nessuna nota"),
+            new Animal("q004w","Nocciola","Caprino",new Date(2018,2,6,0,0,0),"Femmina",8,1,71,[70,71,72,71,69,70],"Nessuna nota"),
+            new Animal("q005w","Noce","Caprino",new Date(2017,9,2,0,0,0),"Femmina",14,3,70,[70,71,71,81,69,70],"Nessuna nota"),
+            new Animal("q006w","Neve","Caprino",new Date(2020,4,6,0,0,0),"Femmina",0,0,69,[70,71,71,81,69,70],"Nessuna nota"),
+            new Animal("q007w","Biancaneve","Caprino",new Date(2020,9,1,0,0,0),"Femmina",0,0,70,[70,71,71,81,69,70],"Nessuna nota"),
+            new Animal("q008w","Brontolo","Caprino",new Date(2020,9,6,0,0,0),"Maschio",0,0,70,[70,71,71,81,69,70],"Nessuna nota"),
+            new Animal("q009w","Mammolo","Caprino",new Date(2017,9,6,0,0,0),"Maschio",0,0,72,[70,71,71,81,69,70],"Nessuna nota"),
+            new Animal("q010w","Cenerentola","Ovino",new Date(2017,9,6,0,0,0),"Femmina",13,3,70,[70,71,71,81,69,70],"Nessuna nota"),
+            new Animal("q011w","Alice","Ovino",new Date(2016,9,6,0,0,0),"Femmina",19,4,73,[70,71,71,81,69,70],"Nessuna nota"),
+            new Animal("q012w","Dolly","Ovino",new Date(2017,10,6,0,0,0),"Femmina",23,3,68,[70,71,71,81,69,70],"Nessuna nota"),
+            new Animal("q013w","Aladino","Ovino",new Date(2016,5,1,0,0,0),"Maschio",0,0,0,[70,71,71,81,59,41],"Nessuna nota"),
+            new Animal("q014w","Jasmine","Ovino",new Date(2020,9,6,0,0,0),"Femmina",0,0,70,[70,71,71,81,69,70],"Nessuna nota"),
+            new Animal("q015w","Bianchina","Ovino",new Date(2020,1,6,0,0,0),"Femmina",0,0,70,[70,71,71,81,69,70],"Nessuna nota")
         ];   
     }
 
@@ -52,7 +53,8 @@ class Register {
     heartBeatLog(){
         var list = [];
         this.list.forEach(e => {
-            var item = {gps: e.tracker, name: e.name, type: e.type, beat: e.hrtbt};
+            var btn = '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="'+e.tracker+'">Dettagli</button>';
+            var item = {gps: e.tracker, name: e.name, type: e.type, beat: e.hrtbt, details: btn};
             list.push(item);
         });
 
@@ -60,6 +62,11 @@ class Register {
     }
 
     getAnimal(gps) {
-        //return this.list.map();       
+        for (var i = 0; i < this.list.length; i++) {
+            if(this.list[i].tracker == gps)
+                return this.list[i];
+        }
+        
+        return undefined;
     }
 }
--- src/main/resources/static/js/loadTable.js
+++ src/main/resources/static/js/loadTable.js
@@ -15,9 +15,18 @@ $( document ).ready(function() {
         .append('<td>'+el.name+'</td>')
         .append('<td>'+el.type+'</td>')
         .append('<td>'+el.beat+' bps</td>')
-        .append('<td>blabla</td>');
+        .append('<td>'+el.details+'</td>');
         
         //inseriscila riga nella tabella
         $("#heart").find('tbody').append(tr);
     });
+
+    //pager
+    let nPg = (list.length/10)
+    for (var i = 0; i < nPg; i++)  {
+        
+        var btn = i==0 ? '<li class="page-item active"><a class="page-link" href="#">'+(i+1)+'</a></li>' : '<li class="page-item"><a class="page-link" href="#">'+(i+1)+'</a></li>'
+            
+        $('.pagination').append(btn)
+    }
 });
