243ccf3c06baefe621eee511b5cd65eced06b9d0
Claudiabuono <52284939+Claudiabuono@users.noreply.github.com>
2020-06-08 20:50:35 +0200

    ricerca per nome e fine implementazione della paginazione

--- src/main/resources/static/js/loadTable.js
+++ src/main/resources/static/js/loadTable.js
@@ -14,20 +14,11 @@
             //appendi tutti i dati
             cols.each(function () {
                 var attr = $(this).attr("name")
-                tr.append('<td>'+el[attr]+'</td>')
+                tr.append('<td name ="'+attr+'">'+el[attr]+'</td>')
             });
             
             //inserisci la riga nella tabella
             $("#heart").find('tbody').append(tr);
             $("#register").find('tbody').append(tr);
         });
-
-        //pager
-        let nPg = (list.length/10)
-        for (var i = 0; i < nPg; i++)  {
-            
-            var btn = i==0 ? '<li class="page-item active"><a class="page-link" href="#">'+(i+1)+'</a></li>' : '<li class="page-item"><a class="page-link" href="#">'+(i+1)+'</a></li>'
-                
-            $('.pagination').append(btn)
-        }
     }
