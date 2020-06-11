a5586769e7692d0200edf279df99d2b204206cd0
Arkemus2302 <g.delmonaco1@studenti.unisa.it>
2020-06-11 08:55:44 +0200

    Formattato le date di nascita degli animali

--- src/main/resources/static/js/animal.js
+++ src/main/resources/static/js/animal.js
@@ -20,21 +20,21 @@ class Animal {
 class Register {
     constructor () {
         this.list =  [
-            new Animal("q001w","Nuvola","Bovino",new Date(2018,9,9,0,0,0),"Femmina",12,3,70,[70,71,71,81,69,70], "Nuvola ha subito una operazione da poco. Potrebbe avere battiti alti"),
-            new Animal("q002w","Pistacchio","Bovino",new Date(2017,9,6,0,0,0),"Maschio",0,0,69,[70,71,71,81,69,70],"Nessuna nota"),
-            new Animal("q003w","Mandorla","Bovino",new Date(2019,9,6,0,0,0),"Femmina",9,1,71,[70,71,71,81,69,70],"Nessuna nota"),
-            new Animal("q004w","Nocciola","Caprino",new Date(2018,2,6,0,0,0),"Femmina",8,1,71,[70,71,72,71,69,70],"Nessuna nota"),
-            new Animal("q005w","Noce","Caprino",new Date(2017,9,2,0,0,0),"Femmina",14,3,70,[70,71,71,81,69,70],"Nessuna nota"),
-            new Animal("q006w","Neve","Caprino",new Date(2020,4,6,0,0,0),"Femmina",0,0,69,[70,71,71,81,69,70],"Nessuna nota"),
-            new Animal("q007w","Biancaneve","Caprino",new Date(2020,9,1,0,0,0),"Femmina",0,0,70,[70,71,71,81,69,70],"Nessuna nota"),
-            new Animal("q008w","Brontolo","Caprino",new Date(2020,9,6,0,0,0),"Maschio",0,0,70,[70,71,71,81,69,70],"Nessuna nota"),
-            new Animal("q009w","Mammolo","Caprino",new Date(2017,9,6,0,0,0),"Maschio",0,0,72,[70,71,71,81,69,70],"Nessuna nota"),
-            new Animal("q010w","Cenerentola","Ovino",new Date(2017,9,6,0,0,0),"Femmina",13,3,70,[70,71,71,81,69,70],"Nessuna nota"),
-            new Animal("q011w","Alice","Ovino",new Date(2016,9,6,0,0,0),"Femmina",19,4,73,[70,71,71,81,69,70],"Nessuna nota"),
-            new Animal("q012w","Dolly","Ovino",new Date(2017,10,6,0,0,0),"Femmina",23,3,68,[70,71,71,81,69,70],"Nessuna nota"),
-            new Animal("q013w","Aladino","Ovino",new Date(2016,5,1,0,0,0),"Maschio",0,0,0,[70,71,71,81,59,41],"Nessuna nota"),
-            new Animal("q014w","Jasmine","Ovino",new Date(2020,9,6,0,0,0),"Femmina",0,0,70,[70,71,71,81,69,70],"Nessuna nota"),
-            new Animal("q015w","Bianchina","Ovino",new Date(2020,1,6,0,0,0),"Femmina",0,0,70,[70,71,71,81,69,70],"Nessuna nota")
+            new Animal("q001w","Nuvola","Bovino",new Date(2018,9,9).toDateString(),"Femmina",12,3,70,[70,71,71,81,69,70], "Nuvola ha subito una operazione da poco. Potrebbe avere battiti alti"),
+            new Animal("q002w","Pistacchio","Bovino",new Date(2017,9,6).toDateString(),"Maschio",0,0,69,[70,71,71,81,69,70],"Nessuna nota"),
+            new Animal("q003w","Mandorla","Bovino",new Date(2019,9,6).toDateString(),"Femmina",9,1,71,[70,71,71,81,69,70],"Nessuna nota"),
+            new Animal("q004w","Nocciola","Caprino",new Date(2018,2,6).toDateString(),"Femmina",8,1,71,[70,71,72,71,69,70],"Nessuna nota"),
+            new Animal("q005w","Noce","Caprino",new Date(2017,9,2).toDateString(),"Femmina",14,3,70,[70,71,71,81,69,70],"Nessuna nota"),
+            new Animal("q006w","Neve","Caprino",new Date(2020,4,6).toDateString(),"Femmina",0,0,69,[70,71,71,81,69,70],"Nessuna nota"),
+            new Animal("q007w","Biancaneve","Caprino",new Date(2020,9,1).toDateString(),"Femmina",0,0,70,[70,71,71,81,69,70],"Nessuna nota"),
+            new Animal("q008w","Brontolo","Caprino",new Date(2020,9,6).toDateString(),"Maschio",0,0,70,[70,71,71,81,69,70],"Nessuna nota"),
+            new Animal("q009w","Mammolo","Caprino",new Date(2017,9,6).toDateString(),"Maschio",0,0,72,[70,71,71,81,69,70],"Nessuna nota"),
+            new Animal("q010w","Cenerentola","Ovino",new Date(2017,9,7).toDateString(),"Femmina",13,3,70,[70,71,71,81,69,70],"Nessuna nota"),
+            new Animal("q011w","Alice","Ovino",new Date(2016,9,6).toDateString(),"Femmina",19,4,73,[70,71,71,81,69,70],"Nessuna nota"),
+            new Animal("q012w","Dolly","Ovino",new Date(2017,10,8).toDateString(),"Femmina",23,3,68,[70,71,71,81,69,70],"Nessuna nota"),
+            new Animal("q013w","Aladino","Ovino",new Date(2016,5,1).toDateString(),"Maschio",0,0,0,[70,71,71,81,59,41],"Nessuna nota"),
+            new Animal("q014w","Jasmine","Ovino",new Date(2020,9,6).toDateString(),"Femmina",0,0,70,[70,71,71,81,69,70],"Nessuna nota"),
+            new Animal("q015w","Bianchina","Ovino",new Date(2020,1,6).toDateString(),"Femmina",0,0,70,[70,71,71,81,69,70],"Nessuna nota")
         ];   
     }
 
